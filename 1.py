f1ea59b98895a10d353a013444382936b2fd5e16
akhilu1890@gmail.com <akhilu1890@gmail.com>
2023-03-18 12:40:44 +0530

    my first commit

---
+++ 1.py
@@ -0,0 +1,47 @@
+a = 10
+print(id(a), a)
+
+a = 20
+print(id(a), a)
+s = "hello"
+print(s[4])
+s1 = "hello"
+print(len(s1))
+a1 = 'hello world'
+print(len(a1))
+print(a1[-2])
+print(a1[-3])
+
+akhil = 'hacking world'
+print(akhil)
+akhi = """ethical hacking
+today no class
+but rain fall"""
+print(akhi)
+print('a')
+print(ord('a'))
+s = 'hello world'
+print(s[4])
+print([6])
+print(s[6])
+print(s[-2])
+s3 = " "
+print(len(s3))
+e = "  "
+print(len(e))
+print(id(a),a)
+a = 30
+print(id(a), a)
+m =100.9
+print(m, id(m))
+print(a, id(a))
+x,y = 25,35
+print(x)
+print(y)
+x,y,z = 10,20,30
+print(x, y, z)
+
+s = "python programming"
+print(s[1: 6: 1])
+
+
